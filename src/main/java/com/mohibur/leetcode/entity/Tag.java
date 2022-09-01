02fafd39b3147f0e080ba5628b11055c34420cc6
Mohibur Rahman <mohiburrahman5509@gmail.com>
2022-09-01 19:08:53 +0600

    Update Tag.java

--- src/main/java/com/mohibur/leetcode/entity/Tag.java
+++ src/main/java/com/mohibur/leetcode/entity/Tag.java
@@ -1,9 +1,12 @@
 package com.mohibur.leetcode.entity;
 
 import lombok.*;
+import org.hibernate.Hibernate;
 
 import javax.persistence.*;
+import java.io.Serializable;
 import java.util.List;
+import java.util.Objects;
 
 @Getter
 @Setter
@@ -11,7 +14,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @Entity
 @Table(name = "tag")
-public class Tag {
+public class Tag implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -20,4 +23,17 @@ public class Tag {
     @JoinTable(name = "tag_discuss", joinColumns = @JoinColumn(name = "tag_id"), inverseJoinColumns = @JoinColumn(name = "discuss_id"))
     @ToString.Exclude
     private List<Discuss> discusses;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Tag tag = (Tag) o;
+        return id != null && Objects.equals(id, tag.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
