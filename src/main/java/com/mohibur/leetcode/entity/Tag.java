ec7c33adf228df6b8c9edde02160b5c39de0da9a
Mohibur Rahman <mohiburrahman5509@gmail.com>
2022-09-18 19:03:40 +0600

    JsonIgnoreProperties added to Tag class

--- src/main/java/com/mohibur/leetcode/entity/Tag.java
+++ src/main/java/com/mohibur/leetcode/entity/Tag.java
@@ -1,5 +1,6 @@
 package com.mohibur.leetcode.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 import org.hibernate.Hibernate;
 
@@ -19,9 +20,13 @@ public class Tag implements Serializable {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(unique = true)
+    private String tagName;
+
     @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JoinTable(name = "tag_discuss", joinColumns = @JoinColumn(name = "tag_id"), inverseJoinColumns = @JoinColumn(name = "discuss_id"))
     @ToString.Exclude
+    @JsonIgnoreProperties({"commentList", "tagList", "problem"})
     private List<Discuss> discusses;
 
     @Override
